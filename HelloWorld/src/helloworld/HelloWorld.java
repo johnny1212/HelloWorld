84eda9f436cb31b417c015c33380d94ca1030121
johnny1212 <johnny1212@volny.cz>
2013-11-14 17:49:52 +0100

    first

---
+++ HelloWorld/src/helloworld/HelloWorld.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld;
+
+/**
+ *
+ * @author Johnny
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
