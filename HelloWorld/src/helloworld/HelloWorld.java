a303d8b9262cbec8a104ad0a1988234dd04eaaaa
johnny1212 <johnny1212@volny.cz>
2013-11-14 18:15:08 +0100

    Last

--- HelloWorld/src/helloworld/HelloWorld.java
+++ HelloWorld/src/helloworld/HelloWorld.java
@@ -7,6 +7,6 @@ package helloworld;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello World! 2");
+        System.out.println("Hello World! 2222");
     }
 }
