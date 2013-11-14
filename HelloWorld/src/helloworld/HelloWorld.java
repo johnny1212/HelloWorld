fe63c509d6d7df56908bbadc54ff26e28c37b88d
johnny1212 <johnny1212@volny.cz>
2013-11-14 17:58:18 +0100

    second

--- HelloWorld/src/helloworld/HelloWorld.java
+++ HelloWorld/src/helloworld/HelloWorld.java
@@ -11,6 +11,6 @@ package helloworld;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hello World!");
     }
 }
